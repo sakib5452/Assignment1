d9761177e22965aa3853aa8c745452332eb6e85b
sakib5452 <najmus191@gmail.com>
2023-11-23 08:14:21 +0600

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+import java.util.Scanner
+fun main(){
+//  Problem:1
+    var ss = Scanner(System.`in`)
+    var mark:Int = ss.nextInt()
+    if(mark>=0 && mark<=32){
+        println("Your grade is : F")
+    }
+    else if(mark>=33 && mark<=39 ){
+        println("Your grade is : D")
+    }
+    else if (mark>=40 && mark<=49){
+        println("Your grade is : C")
+    }
+    else if (mark>=50 && mark<=59){
+        println("Your grade is : B")
+    }
+    else if (mark>=60 && mark<=69){
+        println("Your grade is : A-")
+    }
+    else if (mark>=70 && mark<=79){
+        println("your grade is : A")
+    }
+    else if (mark>=80 && mark<=89 && mark<=100){
+        println("Your grade is : A+")
+    }
+    else{
+        println("Invalid grade!")
+    }
+}
